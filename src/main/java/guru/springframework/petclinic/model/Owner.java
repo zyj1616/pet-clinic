b8dbd7271b17b20ba9846691fa493d6da789ab58
y1zhou <y1zhou@ucsd.edu>
2019-05-22 20:23:36 -0700

    "Pojo Data Model"

---
+++ src/main/java/guru/springframework/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.springframework.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/guru/springframework/petclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package guru.springframework.petclinic.model;
+
+public class Vet extends Person {
+
+}
