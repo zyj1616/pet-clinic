9f6d1dfc3d15ed0ad782822935bba09ca7f472ec
y1zhou <y1zhou@ucsd.edu>
2019-05-23 15:48:56 -0700

    "refactor services"

--- pet-clinic-data/src/main/java/springframework/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/springframework/petclinic/services/OwnerService.java
@@ -2,13 +2,7 @@ package springframework.petclinic.services;
 
 import springframework.petclinic.model.Owner;
 
-import java.util.Set;
+public interface OwnerService extends CrudService<Owner, Long> {
 
-public interface OwnerService {
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
+    Owner findByLastName(String name);
 }
--- pet-clinic-data/src/main/java/springframework/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/springframework/petclinic/services/PetService.java
@@ -2,13 +2,6 @@ package springframework.petclinic.services;
 
 import springframework.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long>{
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
