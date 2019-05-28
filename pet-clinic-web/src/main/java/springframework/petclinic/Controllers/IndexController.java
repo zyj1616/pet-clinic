9e9b6cbb3c09fa7d77df5c34cfc94bb6db2a360f
y1zhou <y1zhou@ucsd.edu>
2019-05-27 17:12:58 -0700

    "finish message 121"

--- pet-clinic-web/src/main/java/springframework/petclinic/Controllers/IndexController.java
+++ pet-clinic-web/src/main/java/springframework/petclinic/Controllers/IndexController.java
@@ -1,14 +1,14 @@
 package springframework.petclinic.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
     @RequestMapping({"", "/", "index", "index.html"})
-    public String index() {
-
+    public String index(Model model) {
         return "index";
     }
 }
